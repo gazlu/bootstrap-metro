32cefe90f66a476dd22f43a15420dea1529406ac
Sushant Kulkarni <sushskulkarni@gmail.com>
2012-07-09 21:51:33 +0530

    Update master

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:81/streetlight/';
+$config['base_url']	= 'http://localhost/bootstrap-metro/';
 
 /*
 |--------------------------------------------------------------------------
